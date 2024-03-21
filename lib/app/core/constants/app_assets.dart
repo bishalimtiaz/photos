7dbabe1db53c0986ee3b1ecb32e35d2d30b9c963
Bishal Imtiaz <bishal.imtiaz@brainstation-23.com>
2024-03-21 22:50:04 +0600

    Add Constant Classes and Assets

---
+++ lib/app/core/constants/app_assets.dart
@@ -0,0 +1,5 @@
+final class AppAssets {
+  static const String imagePath = 'assets/images';
+
+  static const String gallery = '$imagePath/gallery.png';
+}
---
+++ lib/app/core/constants/app_values.dart
@@ -0,0 +1,6 @@
+final class AppValues {
+  static const double dimen_0 = 0;
+  static const double dimen_2 = 2;
+  static const double dimen_4 = 4;
+  static const double dimen_5 = 5;
+}
