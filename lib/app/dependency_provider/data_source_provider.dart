31232c07aec017348ca0f89aa0913f0d0b61f580
Bishal Imtiaz <bishal.imtiaz@brainstation-23.com>
2024-03-21 22:14:10 +0600

    Implement Dependency Provider     - It will handle the registration and unregistration of the dependencies

---
+++ lib/app/dependency_provider/data_source_provider.dart
@@ -0,0 +1,9 @@
+import 'package:get_it/get_it.dart';
+import 'package:photos/app/dependency_provider/provider.dart';
+
+class DataSourceProvider implements Provider {
+  @override
+  Future<void> provide(GetIt locator) async {
+   //TODO: Provide Registration For Your Datasource
+  }
+}
---
+++ lib/app/dependency_provider/dependency_provider.dart
@@ -0,0 +1,79 @@
+import 'package:get_it/get_it.dart';
+import 'package:photos/app/core/utils/log.dart';
+import 'package:photos/app/dependency_provider/data_source_provider.dart';
+import 'package:photos/app/dependency_provider/repository_provider.dart';
+import 'package:photos/app/dependency_provider/service_provider.dart';
+import 'package:photos/app/routes/route_bindings/binding.dart';
+import 'package:photos/app/routes/route_bindings/route_binding.dart';
+
+GetIt locator = GetIt.instance;
+
+class DependencyProvider {
+  DependencyProvider._internal();
+
+  static final DependencyProvider _instance = DependencyProvider._internal();
+
+  factory DependencyProvider() => _instance;
+
+  Future<void> provideDI() async {
+    DataSourceProvider().provide(locator);
+    RepositoryProvider().provide(locator);
+    ServiceProvider().provide(locator);
+  }
+
+  Future<void> provideSharedController<T extends Object>(
+    T Function() controller,
+  ) async {
+    locator.registerLazySingleton(controller);
+  }
+
+  void provideScreenController<T extends Object>(
+    T Function() controller, {
+    required bool isSingleInstance,
+  }) {
+    if (isSingleInstance) {
+      try {
+        if (!locator.isRegistered<T>()) {
+          locator.registerLazySingleton(controller);
+        }
+      } catch (e) {
+        Log.error("Error registering Singleton Controller: $e");
+      }
+    } else {
+      try {
+        if (!locator.isRegistered<T>()) {
+          locator.registerFactory(controller);
+        }
+      } catch (e) {
+        Log.error("Error registering Factory Controller: $e");
+      }
+    }
+  }
+
+  void removeController<T extends Object>() {
+    try {
+      if (locator.isRegistered<T>()) {
+        locator.unregister<T>();
+      }
+    } catch (e) {
+      Log.error("Error removing Screen Controller: $e");
+    }
+  }
+
+  void provideControllerDependency<T extends Object>(
+    T Function() controller,
+  ) {
+    if (!locator.isRegistered<T>()) {
+      locator.registerFactory(controller);
+    }
+  }
+
+  bool canDispose(String? routeName) {
+    Binding? binding = routeBindings[routeName]?.call();
+    if (binding != null && !binding.isSingleInstance) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
